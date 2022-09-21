72b7381441f653ddcb97442247222a27865f6e38
Anup <anuppoudel60@gmail.com>
2022-09-21 21:51:30 +1000

    contact form added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,14 @@ import "./styles/app.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Home from "./components/home/Home";
+import Footer from "./components/layout/Footer";
+import Contact from "./components/contact/Contact.jsx";
+
 import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/menu.scss";
 import "./styles/footer.scss";
-import Footer from "./components/layout/Footer";
+import "./styles/contact.scss";
 
 const App = () => {
   return (
@@ -15,6 +18,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
