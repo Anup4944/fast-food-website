ee9be874d50d01ab0e56f7f4181418dafe68b8d9
Anup <anuppoudel60@gmail.com>
2022-10-04 13:34:04 +1100

    errorhandler, errorMiddleware, googleAuth and user schema created

--- server/app.js
+++ server/app.js
@@ -1,10 +1,40 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectPassport } from "./utils/Provider.js";
+import session from "express-session";
+import passport from "passport";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 dotenv.config({
   path: "./config/config.env",
 });
+
+// Using middlewares
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(cookieParser());
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+connectPassport();
+
+// Import routes
+
+import userRouter from "./routes/user.js";
+
+app.use("/api/v1", userRouter);
+
+// Using error middleware
+app.use(errorMiddleware);
 
 export default app;
